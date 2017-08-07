ad232472681f304b6c108d28ee45d80b806a85eb
Peter Cheng <teed7334@gmail.com>
2017-08-07 19:41:38 +0800

    設定Heroku資料庫連線

--- cyber/settings.py
+++ cyber/settings.py
@@ -94,6 +94,7 @@ WSGI_APPLICATION = 'cyber.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+import dj_database_url
 DATABASES = {
     'default': dj_database_url.config()
 }
